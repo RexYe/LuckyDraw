4d0f05396776d95905e6f36e42e1273d75f48637
Rex Ye <894450343@qq.com>
2017-10-17 17:44:52 +0800

    结构更改

--- js/LuckyDraw.js
+++ js/LuckyDraw.js
@@ -1,6 +1,6 @@
 //选项高亮增加样式 Highlight   中奖选项增加样式 Selected
 //localStorage存储抽奖信息，关闭页面仍保留
-
+//禁止多次单击抽奖按钮.
 function inherit(proto) {
 	//proto是一个对象，但不能是null
 	if(proto == null) throw TypeError();
@@ -20,17 +20,19 @@ Block.prototype.showResult = function() {
 }
 function LuckyDraw(o) {
 	this.o = {
-		round: o.round,
-		sum: o.sum, //基数
-		speed: o.speed,//
+		round: o.round,//圈数
+		sum: o.sum, //总份数
+		speed: o.speed,//初始速度
 		chance: o.chance,//抽奖机会数
+		index: o.index,//初始位置
 	};
-	this.chooseIndex = 3;//获奖位置，随机产生
+	this.flag = false;//结束转动标志
+	this.chooseIndex = 3;//获奖位置编号，随机产生
+	this.step = 0;//步数
 	this.no = 1; //转动过程中位置
-	Block.call(this);
+	// Block.call(this);
+	this.success = function(){}//成功后回调函数
 	this.move = function() {
-		this.setSpeed();
-		let round = this.round;
 		let timer = setInterval( () => {
 			this.addStyle(this.no);//添加高亮
 			this.clearStyle(this.no);//去除高亮
@@ -40,6 +42,7 @@ function LuckyDraw(o) {
 			}
 			this.no++;
 		},this.o.speed)
+		this.step ++;
 	}
 	this.endMove = function(){
 		clearInterval(timer);
@@ -59,18 +62,39 @@ function LuckyDraw(o) {
 	this.getRandom = function(){
 		let ran = Math.floor(Math.random()*this.o.sum);
 		if(ran<1){
-			this.chooseIndex = 2;//一等奖(1)
+			return this.chooseIndex = 2;//一等奖(1)
+			alert(1)
 		}
-		if(0<ran<3){
-			this.chooseIndex = 4;//二等奖(2)
+		if(ran>0 && ran<3){
+			return this.chooseIndex = 4;//二等奖(2)
+			alert(2)
  		}
-		if(2<ran<8){
-			this.chooseIndex = 6;//三等奖(5)
+		else if(ran>2 && ran<8){
+			return this.chooseIndex = 6;//三等奖(5)
+			alert(3)
 		}
-		if(7<ran<18){
-			this.chooseIndex = 8;//四等奖(10)
+		else if(ran>7 && ran<18){
+			return this.chooseIndex = 8;//四等奖(10)
+			alert(4)
 		}
-		console.log(ran);
+		else{
+			let ran2 = Math.ceil(Math.random()*7);
+			if(ran2%2 === 0){
+				ran2 += 1;
+			}
+			if(ran2>7){
+				ran2 -= 1;
+			}
+			let noprize = ran2;
+			return this.chooseIndex = noprize;
+		}
+	}
+	 /**
+     *@function : 抽奖类的初始化操作
+     **/
+	this.init = function(){
+		this.getRandom();
+		console.log(this.chooseIndex)
 	}
 
 }
@@ -87,7 +111,7 @@ console.log(luckyDraw)
 //开始
 function begin() {
 	// luckyDraw.move();
-	luckyDraw.getRandom();
+	luckyDraw.init();
 }
 
 
